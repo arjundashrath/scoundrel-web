077c5878e87352418cd8a715e7186950af7d02f9
Arjun Dashrath <arjundashrath@Arjuns-MacBook-Air.local>
2025-12-16 20:07:55 +0530

    add android data

---
+++ app/src/main/java/com/arjundashrath/scoundrel/DelegationService.java
@@ -0,0 +1,14 @@
+package com.arjundashrath.scoundrel;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
